acd642596684e7b082f4036e786827ab04ce9f32
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-07-24 14:24:40 +0100

    fix: wrapped editor in a Suspense

---
+++ src/app/editor/EditorPageCient.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { Editor } from "@/components/dynamic_editor";
+import { useSearchParams } from "next/navigation";
+
+export default function EditorPageClient() {
+  const searchParams = useSearchParams();
+  const roomId = searchParams.get("roomId");
+  const userName = searchParams.get("username");
+
+  if (!roomId || !userName) {
+    return (
+      <main className="p-8">
+        <h1 className="text-2xl font-bold mb-4">Error</h1>
+        <p className="text-red-500">
+          Missing roomId or username in query parameters.
+        </p>
+      </main>
+    );
+  }
+
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Editor</h1>
+      <Editor
+        roomId={roomId}
+        websocketUrl="ws://144.91.115.254:1234"
+        userName={userName}
+      />
+    </main>
+  );
+}
--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,31 +1,10 @@
-"use client";
-import { Editor } from "@/components/dynamic_editor";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import EditorPageClient from "./EditorPageCient";
 
-export default function Home() {
-  const searchParams = useSearchParams();
-  const roomId = searchParams.get("roomId");
-  const userName = searchParams.get("username");
-
-  if (!roomId || !userName) {
-    return (
-      <main className="p-8">
-        <h1 className="text-2xl font-bold mb-4">Error</h1>
-        <p className="text-red-500">
-          Missing roomId or username in query parameters.
-        </p>
-      </main>
-    );
-  }
-
+export default function EditorPage() {
   return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Editor</h1>
-      <Editor
-        roomId={roomId}
-        websocketUrl="ws://144.91.115.254:1234"
-        userName={userName}
-      />
-    </main>
+    <Suspense fallback={<div className="p-8">Loading editor...</div>}>
+      <EditorPageClient />
+    </Suspense>
   );
 }
